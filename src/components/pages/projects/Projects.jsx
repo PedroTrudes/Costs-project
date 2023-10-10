e57848cae9d54f6c63630619218d0094257eebd8
PedroTrudes <pedro.jtrudes@gmail.com>
2023-10-10 08:21:14 -0300

    created structure of page Projects

--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -2,6 +2,10 @@ import { useLocation } from "react-router-dom";
 
 import Message from "../../layout/message/Message";
 
+import styles from './Projects.module.css';
+import Container from '../../layout/container/Container';
+import LinkButton from "../../button/LinkButton";
+
 function Projects() {
     const location = useLocation()
 
@@ -11,9 +15,15 @@ function Projects() {
         message = location.state.message
     }
     return(
-        <div>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
             <h1>Meus projetos</h1>
+            <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
             {message && <Message msg={message} type="success" />}
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
         </div>
     )
 }
